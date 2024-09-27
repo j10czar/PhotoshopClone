fb327a10b22ca0fd38906ea64e7244af8dfefef9
j10 <jasontenczar@gmail.com>
2024-09-27 17:04:02 -0400

    used different image library and added file saving functionality

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,33 @@
-use embedded_graphics::{prelude::*, pixelcolor::Rgb888};
-use tinytga::{Bpp, Compression, DataType, ImageOrigin, RawPixel, RawTga, TgaHeader};
+use image::{DynamicImage, GenericImageView, ImageBuffer, RgbImage};
+use std::path::Path;
+use std::error::Error;
+use std::env;
+
+
+const FILE_NAME: &str = "car.tga";
+const PATH: &str = "/Users/j10/projects/cop3504c/project3/input/";
 
 
 
-//converts img data to a readable vector filled with tuples in the following format:
-//(r,g,b)
-fn toPixelVector(data: &RawTga<'_>){
-
-   //allocate a new pixel vector in memory for rgb tuples 
-    let mut pixel_vector = Vec::new();
-
-    let pixels: Vec<_> = data.pixels().collect();
+fn main()-> Result<(), Box<dyn Error>>{
 
 
-    for pixel in &pixels {
-        let color_data = pixel.color;
-        //perform bitwise operations to get the real pixel values bc tinytga gives differnt values
-        //with differnet channels
-        let red = ((color_data >> 16) & 0xFF) as u8;
-        let green = ((color_data >> 8) & 0xFF) as u8;
-        let blue = (color_data & 0xFF) as u8;
-
-        pixel_vector.push((red,green,blue))
-    }
+    let fullPath = format!("{}{}",PATH,FILE_NAME);
 
 
-    pixel_vector
+//load image data using image crate
+    let userImage = image::open(&Path::new(&fullPath))?;
 
-}
+    let (imageWidth, imageHeight) = userImage.dimensions();
 
+    //convert image data to RGB format
 
+    let mut rgb_data = userImage.to_rgb8();
 
-fn main(){
-
-    // Include an image from a local path as bytes.
-    let data = include_bytes!("../input/car.tga");
-
-    // Create a TGA instance from a byte slice.
-    let img = RawTga::from_slice(data).unwrap();
+    rgb_data.save(&format!("{}{}","../output/edited_",FILE_NAME))?;
 
 
-
-
-    println!("{}","hello")
+   Ok(()) 
 
 
 }
