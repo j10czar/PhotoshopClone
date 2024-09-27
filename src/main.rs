426c147583da554a33bdc426e621ea71c8440512
j10 <jasontenczar@gmail.com>
2024-09-27 18:32:44 -0400

    added more functions (up to task3)

--- src/main.rs
+++ src/main.rs
@@ -5,49 +5,131 @@ use std::env;
 
 
 const FILE_NAME1: &str = "layer1.tga";
-const FILE_NAME2: &str = "pattern1.tga";
+const FILE_NAME2: &str = "pattern2.tga";
+const FILE_NAME3: &str = "text.tga";
 const PATH: &str = "/Users/j10/projects/cop3504c/project3/input/";
-const SAVE_AS: &str = "test1_multiply.tga";
+const SAVE_AS: &str = "task3_multiply_screen.tga";
 
-fn multiply(image: &mut RgbImage){
-    //pixels_mut is an iterator for all the pixel rgb refrences
-    for pixel in image.pixels_mut()
-    {
-        let [r,g,b] = pixel.0;
+fn multiply(image1: &mut RgbImage, image2: &mut RgbImage) -> RgbImage{
+    //checks to see if the images are the same size
+    assert_eq!(image1.dimensions(), image2.dimensions());
+    let (w,h) = image1.dimensions();
 
-        let r = 255 as u8;
-        let g = 0 as u8;
-        let b = 0 as u8;
+    let mut result = RgbImage::new(w,h);
 
-        *pixel = image::Rgb([r,g,b])
+
+    //zips the pixels together so we can iterate through both of them
+    //also iterate through the result so we can assign the respective pixel value at the right spot
+    for (result_pixel, (p1, p2)) in result
+    .pixels_mut()
+    .zip(image1.pixels().zip(image2.pixels())){
+        let [r1,g1,b1] = p1.0;
+        let [r2,g2,b2] = p2.0;
+
+        //multiplies the pixels and devides by 255 to ensure they dont go out of range of the u8
+        //int pixel value
+
+        let r = (r1 as u16 * r2 as u16/255) as u8;
+        let g = (g1 as u16 * g2 as u16/255) as u8;
+        let b = (b1 as u16 * b2 as u16/255) as u8;
+
+        *result_pixel = image::Rgb([r,g,b])
 
     }
+
+    result
+}
+
+//subtracts image2 from image1
+fn subtract(image1: &mut RgbImage, image2: &mut RgbImage) -> RgbImage{
+    //checks to see if the images are the same size
+    assert_eq!(image1.dimensions(), image2.dimensions());
+    let (w,h) = image1.dimensions();
+
+    let mut result = RgbImage::new(w,h);
+
+
+    //zips the pixels together so we can iterate through both of them
+    //also iterate through the result so we can assign the respective pixel value at the right spot
+    for (result_pixel, (p1, p2)) in result
+    .pixels_mut()
+    .zip(image1.pixels().zip(image2.pixels())){
+        let [r1,g1,b1] = p1.0;
+        let [r2,g2,b2] = p2.0;
+
+        
+        //uses the built in rust saturating sub function which automatically floors and ceilings
+        //the values to prevent overflow
+        let r = r1.saturating_sub(r2);
+        let g = g1.saturating_sub(g2);
+        let b = b1.saturating_sub(b2);
+
+        *result_pixel = image::Rgb([r,g,b])
+
+    }
+
+    result
 }
 
 
+fn screen(image1: &mut RgbImage, image2: &mut RgbImage) -> RgbImage{
+    //checks to see if the images are the same size
+    assert_eq!(image1.dimensions(), image2.dimensions());
+    let (w,h) = image1.dimensions();
 
+    let mut result = RgbImage::new(w,h);
+
+
+    //zips the pixels together so we can iterate through both of them
+    //also iterate through the result so we can assign the respective pixel value at the right spot
+    for (result_pixel, (p1, p2)) in result
+    .pixels_mut()
+    .zip(image1.pixels().zip(image2.pixels())){
+        let [r1,g1,b1] = p1.0;
+        let [r2,g2,b2] = p2.0;
+
+        //screen formula 1-(1-A)*(1-B)
+        //remember 1 is a representation of 255
+        
+        let r = (255-(((255-r1 as u16)*(255-r2 as u16))/255)) as u8;
+
+        let g = (255-(((255-g1 as u16)*(255-g2 as u16))/255)) as u8;
+
+        let b = (255-(((255-b1 as u16)*(255-b2 as u16))/255)) as u8;
+
+
+
+        *result_pixel = image::Rgb([r,g,b])
+
+    }
+
+    result
+}
 
 
 fn main()-> Result<(), Box<dyn Error>>{
 
 
-    let fullPath = format!("{}{}",PATH,FILE_NAME1);
+    let fullPath1 = format!("{}{}",PATH,FILE_NAME1);
+
+    let fullPath2 = format!("{}{}",PATH,FILE_NAME2);
+
+    let fullPath3 = format!("{}{}",PATH,FILE_NAME3);
 
 
 //load image data using image crate
-    let userImage = image::open(&Path::new(&fullPath))?;
+    let mut userImage1 = image::open(&Path::new(&fullPath1))?.to_rgb8();
 
-    let (imageWidth, imageHeight) = userImage.dimensions();
+    let mut userImage2 = image::open(&Path::new(&fullPath2))?.to_rgb8();
 
-    //convert image data to RGB format
-
-    let mut rgb_data = userImage.to_rgb8();
+    let mut userImage3 = image::open(&Path::new(&fullPath3))?.to_rgb8();
     
 
-    multiply(&mut rgb_data);
+    let mut mid = multiply(&mut userImage1,&mut userImage2);
 
+    let output = screen(&mut mid, &mut userImage3);
 
-    rgb_data.save(format!("{}{}","../output/",SAVE_AS))?;
+    output.save(format!("{}{}","../output/",SAVE_AS))?;
 
 
    Ok(()) 
