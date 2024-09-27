7f741812c087d7c4339f8fd7e1d961200a519dda
j10 <jasontenczar@gmail.com>
2024-09-27 17:31:00 -0400

    layed groudwork for manip functions

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,34 @@ use std::error::Error;
 use std::env;
 
 
-const FILE_NAME: &str = "car.tga";
+const FILE_NAME1: &str = "layer1.tga";
+const FILE_NAME2: &str = "pattern1.tga";
 const PATH: &str = "/Users/j10/projects/cop3504c/project3/input/";
+const SAVE_AS: &str = "test1_multiply.tga";
+
+fn multiply(image: &mut RgbImage){
+    //pixels_mut is an iterator for all the pixel rgb refrences
+    for pixel in image.pixels_mut()
+    {
+        let [r,g,b] = pixel.0;
+
+        let r = 255 as u8;
+        let g = 0 as u8;
+        let b = 0 as u8;
+
+        *pixel = image::Rgb([r,g,b])
+
+    }
+}
+
+
 
 
 
 fn main()-> Result<(), Box<dyn Error>>{
 
 
-    let fullPath = format!("{}{}",PATH,FILE_NAME);
+    let fullPath = format!("{}{}",PATH,FILE_NAME1);
 
 
 //load image data using image crate
@@ -23,8 +42,12 @@ fn main()-> Result<(), Box<dyn Error>>{
     //convert image data to RGB format
 
     let mut rgb_data = userImage.to_rgb8();
+    
 
-    rgb_data.save(&format!("{}{}","../output/edited_",FILE_NAME))?;
+    multiply(&mut rgb_data);
+
+
+    rgb_data.save(format!("{}{}","../output/",SAVE_AS))?;
 
 
    Ok(()) 
